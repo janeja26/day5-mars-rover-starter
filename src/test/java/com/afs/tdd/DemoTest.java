e2ba9c13e0cebce7ecca7d0e45f9b0c0daa83a1d
janeja26 <jane.zhang@oocl.com>
2025-09-07 10:59:23 +0800

    test:Add test for moving forward when facing West

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -136,7 +136,20 @@ class DemoTest {
         assertEquals(Direction.S, currentLocation.getDirection());
     }
 
-    
+    @Test
+    void should_move_to_neg1_0_W_when_execute_command_M_given_initial_position_0_0_W() {
+        // Given
+        MarsRover marsRover = new MarsRover(new Location(0, 0, Direction.W));
+
+        // When
+        marsRover.executeCommand(Command.M);
+
+        // Then
+        Location currentLocation = marsRover.getCurrentLocation();
+        assertEquals(-1, currentLocation.getX());
+        assertEquals(0, currentLocation.getY());
+        assertEquals(Direction.W, currentLocation.getDirection());
+    }
 
     }
 
